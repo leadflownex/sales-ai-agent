745f35484bed6ec71f26df312231f2aee8e19eae
Mohd Wasil <appfodev666@gmail.com>
2025-11-09 00:14:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ['images.unsplash.com', 'avatars.githubusercontent.com'],
-  },
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/dashboard',
-        permanent: true,
-      },
-    ]
-  },
+  output: 'standalone'
 }
 
 module.exports = nextConfig
